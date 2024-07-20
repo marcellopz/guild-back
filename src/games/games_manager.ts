b688b080b09eff7dde0344faed3b1e318e36f182
kin309 <30506992+kin309@users.noreply.github.com>
2024-07-20 04:57:52 -0300

    other changes

--- src/games/games_manager.ts
+++ src/games/games_manager.ts
@@ -1,9 +1,13 @@
 import HashGame from "./hash_game/hash_game";
+import Player from "./player";
 
 class GamesManager{
+    public players : Array<Player> = new Array<Player>(new Player("P1"), new Player("P2"))
     public hash_game : HashGame
 
     constructor(){
-        this.hash_game = new HashGame()
+        this.hash_game = new HashGame(this.players)
     }
 }
+
+export default GamesManager
--- src/games/hash_game/hash_game.ts
+++ src/games/hash_game/hash_game.ts
@@ -1,10 +1,30 @@
+import Player from "games/player";
 import Hash from "./hash";
+import TurnManager from "./turn_manager";
+import HashGameRule from "./hash_game_rule";
 
 class HashGame{
-    public hash : Hash = new Hash();
-    // players
+    private _hash : Hash = new Hash();
+    private _turn_manager : TurnManager;
+    private _players : Array<Player>;
+    private _rule : HashGameRule;
     // game_rules
-    // turn_manager
+
+    constructor(players:Array<Player>){
+        this._turn_manager = new TurnManager(players)
+        this._rule = new HashGameRule(this);
+        this._players = players;
+        this._rule.addConditionMetListener(this.on_player_win)
+
+    }
+
+    public get_hash(): Hash{
+        return this._hash;
+    }
+
+    public on_player_win(){
+        // the actual player wins
+    }
 }
 
 export default HashGame;
