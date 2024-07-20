8ccbcd38dfd84218505692452fcc9344e00cedb6
kin309 <30506992+kin309@users.noreply.github.com>
2024-07-20 04:55:38 -0300

    implement gamerules

---
+++ src/games/game_rule.ts
@@ -0,0 +1,35 @@
+import Player from "./player";
+
+class GameRule<T>{
+    private _game: T;
+    private _ended: boolean = false;
+    private _conditionMet: ((value: boolean) => void)[] = [];
+
+    constructor(game:T){
+        this._game = game;
+    }
+
+    get Ended():boolean{
+        return this._ended;
+    }
+
+    public win(){
+        if (!this._ended){
+            this._conditionMet.forEach(callback => callback(true));
+            this._ended = true;
+        }
+    }
+
+    public lose(){
+        if (!this._ended){
+            this._conditionMet.forEach(callback => callback(false));
+            this._ended = true;
+        }
+    }
+
+    public addConditionMetListener(callback: (value: boolean) => void) {
+        this._conditionMet.push(callback);
+    }
+}
+
+export default GameRule;
